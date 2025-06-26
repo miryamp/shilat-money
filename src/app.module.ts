2c7c42893a3dbc18b7ca2beb2459e37bdef8166c
TsvielShilat <tsviel.shilat@gmail.com>
2025-06-26 10:37:35 +0300

    iniitalize global db connection in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // set to false in production
+    }),
+    CategoryModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
