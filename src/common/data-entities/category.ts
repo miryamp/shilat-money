0462272ffff83889a20d8afcf6e075cc4263390a
TsvielShilat <tsviel.shilat@gmail.com>
2025-06-26 09:29:06 +0300

    moved shared files to common folder

---
+++ src/common/data-entities/category.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { TransactionType } from './transaction-type';
+
+@Entity()
+export class Category {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    householdId: string;
+
+    @ManyToOne(() => Household, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'householdId' })
+    household: Household;
+
+    @Column()
+    name: string;
+
+    @Column({
+        type: 'enum',
+        enum: TransactionType
+    })
+    type: TransactionType;
+
+    @Column()
+    color: string; // hex color code
+
+    @Column()
+    icon: string;
+}
