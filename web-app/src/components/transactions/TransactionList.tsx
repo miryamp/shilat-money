9fce54b356b32fb4f097090f339250f77d94abde
Miryam <tsviel.shilat@gmail.com>
2025-07-20 09:39:42 +0300

    removed accidently commited debug log

--- web-app/src/components/transactions/TransactionList.tsx
+++ web-app/src/components/transactions/TransactionList.tsx
@@ -59,8 +59,6 @@ const TransactionList: React.FC<TransactionListProps> = ({
     );
   }
 
-  console.log(sortedTransactions.map(transaction => `${transaction.id}, ${transaction.recurrenceId}`))
-
   return (
     <>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=repeat" />
